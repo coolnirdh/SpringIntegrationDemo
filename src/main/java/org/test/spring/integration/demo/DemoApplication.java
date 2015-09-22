68d625933be43916183523bc51cd5989fd2c3889
Nirdh Mehta <nirdh.r.mehta@tsgforce.com>
2015-09-22 13:28:55 +0530

    Initial commit

---
+++ src/main/java/org/test/spring/integration/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package org.test.spring.integration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:jmsDemo.xml")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+
+	}
+}
